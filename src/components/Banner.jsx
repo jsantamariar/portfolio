e850f07cf478cb8993005c75bede0a2f6b3b6992
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-12 00:25:55 -0500

    refactor: updating description

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
   const toRotate = ["Web Developer", "Mobile Developer", "3D Enthusiastic"];
@@ -19,33 +19,37 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     }
-  }
+  };
 
   return (
     <section className="banner" id="home">
@@ -53,43 +57,63 @@ export const Banner = () => {
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>
                     {`Hi! I'm Jorge `}
                     <br />
-                    <span className="txt-rotate" data-rotate='[ "Web Developer", "Mobile Developer", "3D Enthusiastic" ]'>
+                    <span
+                      className="txt-rotate"
+                      data-rotate='[ "Web Developer", "Mobile Developer" ]'
+                    >
                       <span className="wrap">{text}</span>
                     </span>
                   </h1>
-                  <p>I've worked for US Companies as a frontend UI developer. Here you'll find interesting projects I made. Enjoy it!</p>
-                  <div style={{ display: 'flex', gap: 30 }}>
-
+                  <p>
+                    I've worked for US Companies as a frontend developer. Here
+                    you'll find interesting projects I made. Enjoy it!
+                  </p>
+                  <div style={{ display: "flex", gap: 30 }}>
                     <a href="#connect">
-                      <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} />
+                      <button onClick={() => console.log("connect")}>
+                        Let’s Connect <ArrowRightCircle size={25} />
                       </button>
                     </a>
-                    <a target="_blank" style={{ textDecoration: 'none' }} href="https://bold.pro/my/jorge-santamaria-231128150828" rel="noreferrer">
-                      <button>Portfolio <ArrowRightCircle size={25} />
+                    <a
+                      target="_blank"
+                      style={{ textDecoration: "none" }}
+                      href="https://bold.pro/my/jorge-santamaria-231128150828"
+                      rel="noreferrer"
+                    >
+                      <button>
+                        Portfolio <ArrowRightCircle size={25} />
                       </button>
                     </a>
                   </div>
-
-                </div>}
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
                   <img src={headerImg} alt="Header Img" />
                 </div>
-              }
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
--- src/models/Mac.jsx
+++ src/models/Mac.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import { useGLTF, Html } from '@react-three/drei';
+import React from "react";
+import { useGLTF, Html } from "@react-three/drei";
 
 const Mac = (props) => {
-    const mac = useGLTF('./mac-model.gltf');
+  const mac = useGLTF("./mac-model.gltf");
 
-    return (
-        <primitive {...props} object={mac.scene}>
-            <Html
-                wrapperClass='htmlScreen'
-                transform
-                distanceFactor={2.6}
-                position={[0, 1.51, -1.4]}
-                rotation-x={-0.226}
-            >
-                <iframe src="https://threejs-portfolio-teal.vercel.app/" width={450} height={285} />
-            </Html>
-        </primitive>
-    )
-}
+  return (
+    <primitive {...props} object={mac.scene}>
+      <Html
+        wrapperClass="htmlScreen"
+        transform
+        distanceFactor={2.6}
+        position={[0, 1.51, -1.4]}
+        rotation-x={-0.226}
+      >
+        <iframe
+          title="mac"
+          src="https://threejs-portfolio-teal..app/"
+          width={450}
+          height={285}
+        />
+      </Html>
+    </primitive>
+  );
+};
 
 export default Mac;
